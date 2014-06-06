a16ff88036371f59dee6a5a9d902bac7bbccafb8
nuex <nx@nu-ex.com>
2014-06-06 09:00:01 -0400

    minimal packer support cookbook

---
+++ cookbooks/packer/metadata.rb
@@ -0,0 +1,12 @@
+name 'packer'
+maintainer 'nuex'
+maintainer_email 'nx@nu-ex.com'
+license 'MIT'
+description 'Support for packer'
+
+supports 'ubuntu'
+
+depends 'apt'
+depends 'annoyances'
+depends 'build-essential'
+depends 'hostname'
---
+++ cookbooks/packer/recipes/default.rb
@@ -0,0 +1,8 @@
+# Cookbook Name:: packer
+# Recipe Name:: default
+#
+# Default recipe
+include_recipe 'apt'
+include_recipe 'annoyances'
+include_recipe 'build-essential'
+include_recipe 'hostname'
