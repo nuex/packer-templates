0784080c09160fc35738327f3daa7bdfd0f3db62
nuex <nx@nu-ex.com>
2014-07-22 01:32:17 -0400

    erlang support

---
+++ cookbooks/packer-erlang/metadata.rb
@@ -0,0 +1,10 @@
+name 'packer-erlang'
+maintainer 'nuex'
+maintainer_email 'nx@nu-ex.com'
+license 'MIT'
+description 'Configure a packer image with erlang installed'
+
+supports 'ubuntu'
+
+depends 'packer'
+depends 'erlang'
